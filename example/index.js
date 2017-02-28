f9843c4b20db7bc0db1ab5f4f761114ef93167bc
Vishwakarma <vishwakarma.iiita@@gmail.com>
2017-02-28 19:41:03 +0530

    wreck fixes

--- example/index.js
+++ example/index.js
@@ -1,5 +1,5 @@
 var lp = require('../lib/');
-var url = 'https://www.linkedin.com/in/nelsonic';
+var url = 'https://www.linkedin.com/in/satishkaushik174';
 lp(url, function(err, data){
   console.log(JSON.stringify(data,null,2));
-})
+});
